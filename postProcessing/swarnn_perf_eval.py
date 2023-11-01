ffdf9c1e2eba7e6fae816d99395acfe0d10c510a
Anirban Mukhopadhyay <70462362+anirbanm93@users.noreply.github.com>
2023-11-01 22:04:37 +0530

    Update swarnn_perf_eval.py

--- postProcessing/swarnn_perf_eval.py
+++ postProcessing/swarnn_perf_eval.py
@@ -41,15 +41,14 @@ def vis_feat_rels(df_with_targetlabels: pd.core.frame.DataFrame, target_col_name
         fig.savefig(save_paths[1], dpi=300, bbox_inches='tight')
 
 
-def find_nearest(arr, val, return_indx=False):
+def find_nearest(arr: np.ndarray, val: float, return_indx=False):
     """
-    param arr: a numpy array or list
+    param arr: a numpy array
     param val: a value
     return: return the element of the array which is closest to the value
     """
-    if isinstance(arr, list):
-        arr = np.array(arr)
     indx = np.argmin(np.abs(arr - val))
+
     if return_indx:
         return indx, arr[indx]
     else:
@@ -57,7 +56,8 @@ def find_nearest(arr, val, return_indx=False):
 
 
 def eval_classif(H, y, regr_modelID: int = 0,
-                 testsize: float = 0.20, target_labels=None, plot_conf_mat=False, save_path=None):
+                 testsize: float = 0.20, target_labels: list[str] = None,
+                 plot_conf_mat: bool = False, save_path: str = None):
     """
     param H: Hidden layer output matrix
     param y: Target variable
@@ -83,9 +83,7 @@ def eval_classif(H, y, regr_modelID: int = 0,
     regr.fit(H_train, y_train_enc)
 
     # output weight matrix
-    model = {}
-    model['coeff'] = regr.coef_
-    model['intercept'] = regr.intercept_
+    model = {'coeff': regr.coef_, 'intercept': regr.intercept_}
 
     # Make predictions using the testing set
     output = regr.predict(H_test)
@@ -94,17 +92,19 @@ def eval_classif(H, y, regr_modelID: int = 0,
     y_pred = oheenc.inverse_transform(output)
 
     if plot_conf_mat:
-        im = ConfusionMatrixDisplay.from_predictions(y_test, y_pred, display_labels=target_labels)
+        im = ConfusionMatrixDisplay.from_predictions(y_test.astype(str), y_pred.astype(str),
+                                                     display_labels=target_labels)
         if save_path is not None:
             fig = im.figure_
             fig.savefig(save_path, dpi=300, bbox_inches='tight')
 
-    print(classification_report(y_test, y_pred, labels=target_labels))
+    print(classification_report(y_test.astype(str), y_pred.astype(str), labels=target_labels))
 
     return model
 
 
-def eval_timeseries_regr_cv(regr_modelID: int, H, y, numofFold: int = 5):
+def eval_timeseries_regr_cv(H, y, regr_modelID: int = 0, numofFold: int = 5,
+                            return_test_pred: bool = False, return_model: bool = False):
     """
     param H: Hidden layer output matrix
     param y: Target variable
@@ -115,6 +115,7 @@ def eval_timeseries_regr_cv(regr_modelID: int, H, y, numofFold: int = 5):
     regr = linregr[regr_modelID]()
 
     mse_cv = []
+
     y_pred = {}
     y_test = {}
     coeff = {}
@@ -148,11 +149,19 @@ def eval_timeseries_regr_cv(regr_modelID: int, H, y, numofFold: int = 5):
 
     print('MSE averaged over all the folds:', np.round(np.mean(mse_cv), 2))
 
-    return y_test, y_pred, wout, mse_cv
+    if return_test_pred:
+        return y_test, y_pred, mse_cv
+    elif return_model:
+        return coeff, intercept, mse_cv
+    elif return_test_pred and return_model:
+        return y_test, y_pred, coeff, intercept, mse_cv
+    else:
+        return mse_cv
 
 
 def eval_classif_cv(regr_modelID: int, H, y, numofFold: int = 5,
-                    target_labels=None, plot_conf_mat=False, save_path=None):
+                    target_labels=None, plot_conf_mat=False, save_path=None,
+                    return_test_pred: bool = False, return_model: bool = False):
     """
     Training linear regression models on subsets of the available input data
     and evaluating them on the complementary subset of the data.
@@ -171,9 +180,12 @@ def eval_classif_cv(regr_modelID: int, H, y, numofFold: int = 5,
     accuracy_score_cv = []
     precision_cv = []
     recall_cv = []
-    y_pred = []
-    y_test = []
-    coeff = []
+    f1score_cv = []
+
+    y_pred = {}
+    y_test = {}
+    coeff = {}
+    intercept = {}
 
     skf = StratifiedKFold(n_splits=numofFold, random_state=1, shuffle=True)
     for i, (train_index, test_index) in enumerate(skf.split(H, y)):
@@ -182,7 +194,7 @@ def eval_classif_cv(regr_modelID: int, H, y, numofFold: int = 5,
 
         H_train, H_test = H[train_index], H[test_index]
         y_train, ytest = y[train_index], y[test_index]
-        y_test.append(ytest)
+        y_test['fold-'+str(i)] = ytest
 
         # Source: https://stackoverflow.com/questions/55525195/
         # do-i-have-to-do-one-hot-encoding-separately-for-train-and-test-dataset
@@ -192,39 +204,49 @@ def eval_classif_cv(regr_modelID: int, H, y, numofFold: int = 5,
         regr.fit(H_train, y_train_enc)
 
         # Output weight
-        coeff.append(regr.coef_)
+        coeff['fold-'+str(i)] = regr.coef_
+        intercept['fold-'+str(i)] = regr.intercept_
 
         # Make predictions using the testing set
         output = oheenc.inverse_transform(regr.predict(H_test)).flatten()
 
-        y_pred.append(output)
+        y_pred['fold-'+str(i)] = output
+
+        # default value of beta is 1 for F-beta score
         accuracy_score_cv.append(np.round(balanced_accuracy_score(ytest, output), 2))
         precision_cv.append(np.round(precision_recall_fscore_support(ytest, output, average='weighted')[0], 2))
         recall_cv.append(np.round(precision_recall_fscore_support(ytest, output, average='weighted')[1], 2))
+        f1score_cv.append(np.round(precision_recall_fscore_support(ytest, output, average='weighted')[2], 2))
 
     accuracy_score_cv = np.array(accuracy_score_cv)
     precision_cv = np.array(precision_cv)
     recall_cv = np.array(recall_cv)
-    y_pred = np.array(y_pred, dtype=object)
-    y_test = np.array(y_test, dtype=object)
-    wout = np.array(wout)
+    f1score_cv = np.array(f1score_cv)
 
-    print('Accuracy score averaged over all the folds:', np.round(np.mean(accuracy_score_cv), 2),
-          'Precision averaged over all the folds:', np.round(np.mean(precision_cv), 2),
-          'Recall averaged over all the folds:', np.round(np.mean(recall_cv), 2))
+    print('Precision averaged over all the folds:', np.round(np.mean(precision_cv), 2),
+          'Recall averaged over all the folds:', np.round(np.mean(recall_cv), 2),
+          'F1 score averaged over all the folds:', np.round(np.mean(f1score_cv), 2),
+          'Accuracy score averaged over all the folds:', np.round(np.mean(accuracy_score_cv), 2),)
 
     # Plot confusion matrix for the fold which has maximum accuracy
-    foldindx_maxacc = np.argmax(accuracy_score_cv)
+    i_maxacc = np.argmax(accuracy_score_cv)
 
     if plot_conf_mat:
-        im = ConfusionMatrixDisplay.from_predictions(y_test[foldindx_maxacc, ...].astype(str),
-                                                     y_pred[foldindx_maxacc, ...].astype(str),
+        im = ConfusionMatrixDisplay.from_predictions(y_test['fold-'+str(i_maxacc)].astype(str),
+                                                     y_pred['fold-'+str(i_maxacc)].astype(str),
                                                      display_labels=target_labels)
         if save_path is not None:
             fig = im.figure_
             fig.savefig(save_path, dpi=300, bbox_inches='tight')
 
-    return wout, accuracy_score_cv, precision_cv, recall_cv
+    if return_test_pred:
+        return y_test, y_pred, precision_cv, recall_cv, f1score_cv, accuracy_score_cv
+    elif return_model:
+        return coeff, intercept, precision_cv, recall_cv, f1score_cv, accuracy_score_cv
+    elif return_test_pred and return_model:
+        return y_test, y_pred, coeff, intercept, precision_cv, recall_cv, f1score_cv, accuracy_score_cv
+    else:
+        return precision_cv, recall_cv, f1score_cv, accuracy_score_cv
                       
 
 def feat_lin_scaleto_exp_param(dataset: np.ndarray, exp_param_min: np.ndarray,
@@ -232,10 +254,9 @@ def feat_lin_scaleto_exp_param(dataset: np.ndarray, exp_param_min: np.ndarray,
     """
     param dataset: numpy array where each column represents a feature
     param exp_param_min: Minimum values of the experimental parameters
-
     param exp_param_max: Maximum values of the experimental parameters
     param decimals: Number of decimal places to round to
-    return: transformed dataset where feature space is mapped on to experimental parameter space
+    return: transformed dataset where feature space is mapped onto experimental parameter space
     """
     slope = (exp_param_max - exp_param_min) / (np.amax(dataset, axis=0) - np.amin(dataset, axis=0))
     intercept = exp_param_max - slope * np.amax(dataset, axis=0)
@@ -258,7 +279,7 @@ def nCr(n, r):
     # Initialize the logarithmic sum to 0
     res = 0
 
-    # Calculate the logarithmic sum of the numerator and denominator using loop
+    # Calculate the logarithmic sum of the numerator and denominator
     for i in range(r):
         # Add the logarithm of (n-i) and subtract the logarithm of (i+1)
         res += np.log(n - i) - np.log(i + 1)
@@ -267,17 +288,19 @@ def nCr(n, r):
 
 
 # Calculates number of states for each feature
-def calc_numofFreqBins(numofInstances: int, numofFeats: int):
+def calc_numofBins(numofInstances: int, numofFeats: int):
+    
     val = 0
-    numofFreqBins = numofFeats - 1
+    numofBins = 0
 
-    while val <= numofInstances:
-        numofFreqBins += 1
-        val = nCr(numofFreqBins + numofFeats - 1, numofFeats)
+    while val < numofInstances:
+        numofBins += 1
+        # val = nCr(numofBins + numofFeats - 1, numofFeats)
+        val = numofBins ** numofFeats
 
-    print(f'Number of combinations possible with {numofFreqBins} frequency bins is {val}.')
+    print(f'Number of combinations possible with {numofBins} bins is {val}.')
 
-    return numofFreqBins
+    return numofBins
 
 
 def feat_bininngto_exp_param(dataset: np.ndarray, exp_param_vals: np.ndarray):
@@ -304,20 +327,20 @@ def feat_bininngto_exp_param(dataset: np.ndarray, exp_param_vals: np.ndarray):
     return ds_transformed
 
 
-def find_unique_freqvals(freqvals: np.ndarray):
+def roundoff_uniqarr(vals: np.ndarray):
     """
-    param freqvals: array of frequency values of shape: (num of freq values, num of features) or (num of freq values)
+    param vals: array of values of shape: (num of values, num of features) or (num of values)
     """
     decimal_places = 0
 
     while True:
         # Round the array to the current decimal place
-        rounded_arr = np.round(freqvals, decimals=decimal_places)
+        rounded_arr = np.round(vals, decimals=decimal_places)
 
         # Check if all values are unique at this decimal place
-        if (np.unique(rounded_arr, axis=0)).size < freqvals.size:
+        if (np.unique(rounded_arr, axis=0)).size < vals.size:
             decimal_places += 1
         else:
             break
 
-    return np.round(freqvals, decimals=decimal_places)
+    return np.round(vals, decimals=decimal_places)
